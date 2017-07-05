1037a5363bda60388a189230bd8e88afe9fd399c
mitch <mitchell.negus.57@gmail.com>
2017-07-05 10:27:21 -0400

    initial commit of energy_codes module

---
+++ energy_codes.py
@@ -0,0 +1,37 @@
+"""DOCSTRING comment"""
+
+def name_to_code(name):
+    """
+    Convert an energy source name to its corresponding EIA dataset numeric code.
+    
+    Parameters
+    ----------
+    name : str
+        The name of an EIA energy or energy group.
+        
+    Returns
+    -------
+    code : int
+        The code corresponding to the energy source provided.
+    """
+    key_name = name.lower()
+    Ecodes = {'coal':1,
+              'natural gas':2,
+              'petroleum':3,
+              'fossil fuel':4,
+              'nuclear':5,
+              'hydro':6,
+              'geothermal':7,
+              'solar':8,
+              'wind':9,
+              'biomass':10,
+              'renewable':11,
+              'primary':12}
+        
+    if key_name not in Ecodes:
+        raise KeyError('"%s" was not found in the EIA dataset; \
+                        see documentation for implemented energy sources')
+    else:
+        code = Ecodes[key_name]
+    return code
+
