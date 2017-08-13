3929d637bbfdd420029d9fb447c394b0655fd5f1
mitch <mitchell.negus.57@gmail.com>
2017-08-12 17:59:57 -0700

    more work on Eclass and update of codes module

--- eia_codes.py
+++ eia_codes.py
@@ -4,7 +4,9 @@ Provide utilities for working with EIA dataset codes.
 Functions
 ---------
 name_to_code
-    Converts energy names into EIA energy codes
+    Converts energy names into EIA energy codes.
+date_to_code
+    Converts date string into EIA data code.
 """
 
 def name_to_code(name):
@@ -18,7 +20,7 @@ def name_to_code(name):
         
     Returns
     -------
-    code : int
+    name_code : int
         The code corresponding to the energy source provided.
     """
     key_name = name.lower()
@@ -38,6 +40,47 @@ def name_to_code(name):
     if key_name not in Ecodes:
         raise KeyError('Key "{}" was not found in the EIA dataset; see documentation for implemented energy sources.'.format(key_name))
     else:
-        code = Ecodes[key_name]
-    return code
+        name_code = Ecodes[key_name]
+    return name_code
 
+def date_to_code(date):
+    """
+    Convert an input date to its corresponding EIA dataset numeric date code.
+    
+    Parameters
+    ----------
+    date : str
+        A date, given in the format 'YYYYMM', 'YYYY-MM', or 'MM-YYYY'. 
+        Dashes ("-") can be substituted for periods ("."), underscores ("_"), or forward slashes ("/").
+        
+    Returns
+    -------
+    date_code : int
+        The code corresponding to the energy source provided.
+    """
+    bad_format_err_msg = 'Date "{}" was not given in an acceptable format; try formatting date as "YYYYMM".'.format(date)
+    acceptable_separators = ["-",".","/","_"]
+    
+    # Convert date to code
+    if len(date) == 6:
+        date_code_string = date
+    elif len(date) == 7:
+        if date[4] in acceptable_separators:
+            date_code_string = date.replace(date[4],'')
+        elif date[2] in acceptable_separators:
+            date_code_string = (date[3:]+date[:3]).replace(date[2],'')
+    else:
+        raise ValueError(bad_format_err_msg)
+        
+    # Check reasonability of date provided
+    try:
+        year = int(float(date_code_string[0:4]))
+        month = int(float(date_code_string[4:6]))
+        if year < 1900 or year > 3000:
+            raise ValueError('No data exists for this time period.')
+        if month > 13:  # 13 denotes full year sum
+            raise ValueError('A month must be given as a number 1-12')
+        date_code = int(float(date_code_string))
+    except:
+        raise ValueError(bad_format_err_msg)
+    return date_code
--- test_eia_codes.py
+++ test_eia_codes.py
@@ -1,10 +1,25 @@
 from eia_codes import name_to_code as ntc
-
+from eia_codes import date_to_code as dtc
 
 class TestNTC:
     
-    def test_ntc_code(self):
+    def test_ntc_ConvertingName(self):
         assert ntc('coal') == 1
     
-    def test_ntc_case(self):
+    def test_ntc_IgnoringCase(self):
         assert ntc('Renewable') == 11
+
+class TestDTC:
+
+    def test_dtc_ReadingFormat1(self):
+        assert dtc('20170812') == float(20170812)
+
+    def test_dtc_ReadingFormat2(self):
+        assert dtc('2017-08-12') == float(20170812)
+        
+    def test_dtc_ReadingFormat3(self):
+        assert dtc('08-12-2017') == float(20170812)
+
+    def test_dtc_ReadingFormat4(self):
+        assert dtc('08/12/2017') == float(20170812)
+
