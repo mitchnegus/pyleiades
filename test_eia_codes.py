60dcf5ed40bfa9e1f3716b3ca4dc5d14598761ef
mitch <mitchell.negus.57@gmail.com>
2017-08-12 18:03:19 -0700

    fixed dates (no day needed)

--- test_eia_codes.py
+++ test_eia_codes.py
@@ -12,14 +12,14 @@ class TestNTC:
 class TestDTC:
 
     def test_dtc_ReadingFormat1(self):
-        assert dtc('20170812') == float(20170812)
+        assert dtc('201708') == float(201708)
 
     def test_dtc_ReadingFormat2(self):
-        assert dtc('2017-08-12') == float(20170812)
+        assert dtc('2017-08') == float(201708)
         
     def test_dtc_ReadingFormat3(self):
-        assert dtc('08-12-2017') == float(20170812)
+        assert dtc('08-2017') == float(201708)
 
     def test_dtc_ReadingFormat4(self):
-        assert dtc('08/12/2017') == float(20170812)
+        assert dtc('08/2017') == float(201708)
 
