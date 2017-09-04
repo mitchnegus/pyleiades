29e11efe2d641e1247bf109a2db4586c32314abe
mitch <mitchell.negus.57@gmail.com>
2017-09-04 16:08:35 -0700

    finished majority of eclass

--- main/eclass.py
+++ main/eclass.py
@@ -11,11 +11,12 @@ class EClass:
     Retrieves data from the specified energy source according to specific 
     attributes, such as energy consumed per decade, per year, or all years in 
     which more than a certain amount of energy was consumed from that source.
+    Use this class to extract and return pure data from the dataset.
     """
     
     def __init__(self,energy_source,dataset=np.empty(0)):
         """
-        Receive energy source (as numeric code) and collect corresponding data from the input dataset
+        Receive energy source (as numeric code) and collect corresponding data.
         
         Parameters
         ----------
@@ -42,20 +43,23 @@ class EClass:
         # Get the oldest and newest datapoint dates for this energy" 
         self.idate = int(min(self.data[:,0]))
         self.fdate = int(max(self.data[:,0]))
-    
-    def daterange(self,start_date,end_date):
+        
+        self.freq_errmsg = '"{}" is not a frequency compatible with this dataset; see documentation for permissible frequencies.' 
+        self.extr_errmsg = '"{}" is not a recognized input for an extrema; try "max" or "min"' 
+
+    def _daterange(self,start_date,end_date):
         """
         Resize the dataset to cover only the date range specified.
         
         Parameters
         ----------
         start_date, end_date : int
-            The dataset start/end dates (both inclusive) as integers (YYYYMM)
+            The dataset start/end dates (both inclusive) as integers (YYYYMM).
             
         Returns
         -------
         bound_data : ndarray
-            A 2 column array corresponding to the specified range.
+            A 2 column array corresponding to the specified date range.
         """
         # Use dataset default dates unless otherwise specified by the user
         start_date = self.idate if start_date == None else date_to_code(str(start_date))
@@ -65,8 +69,45 @@ class EClass:
         half_bounded_data = self.data[self.data[:,0] >= start_date]
         bounded_data = half_bounded_data[half_bounded_data[:,0] <= end_date]
         return bounded_data
-        
      
+    def _monthlydata(self,data):
+        """
+        Limit the data to consider only monthly totals.
+
+        Parameters
+        ----------
+        data : ndarray
+            A 2 column array with data to be filtered according to monthly totals.
+
+        Returns
+        -------
+        monthly_data : ndarray
+            A 2 column array corresponding to only the monthly totals.
+        """
+        # Remove dates where the fifth and sixth digits of the date code are not in 01-12
+        month_codes = np.array([int(float(str(date_code)[4:6])) for date_code in data[:,0]])
+        monthly_data = data[month_codes!=13]
+        return monthly_data
+     
+    def _yearlydata(self,data):
+        """
+        Limit the data to consider only yearly totals.
+
+        Parameters
+        ----------
+        data : ndarray
+            A 2 column array with data to be filtered according to yearly totals.
+
+        Returns
+        -------
+        yearly_data : ndarray
+            A 2 column array corresponding to only the yearly totals.
+        """
+        # Remove dates where the fifth and sixth digits of the date code are not 13 (denoting yearly total)
+        month_codes = np.array([int(float(str(date_code)[4:6])) for date_code in data[:,0]])
+        yearly_data = data[month_codes==13]
+        return yearly_data
+       
     def totals(self,freq='yearly',start_date=None,end_date=None,):
         """
         Get the energy consumption totals over a given period.
@@ -88,19 +129,17 @@ class EClass:
         freq = freq.lower()
 
         # Bound data by start and end dates
-        totals_data = self.daterange(start_date,end_date)
+        totals_data = self._daterange(start_date,end_date)
         
         # Extract data according to frequency
-        month_codes = np.array([int(float(str(date_code)[4:])) for date_code in totals_data[:,0]])
         if freq == 'monthly':
-            # Create a numpy array selecting only monthly totals (denoted as YYYYMM where MM is 01-12 in the date_code)
-            totals_data = totals_data[month_codes!=13]
+            totals_data = self._monthlydata(totals_data)
         elif freq == 'yearly':
-            # Create a numpy array selecting only yearly totals (denoted as YYYY13 in the date_code)
-            totals_data = self.data[month_codes==13]
+            totals_data = self._yearlydata(totals_data)
         elif freq == 'cumulative':
             # Create a numpy array (1x2) of the energy source's cumulative total
-            date_indices_to_sum = month_codes==13
+            month_codes = np.array([int(float(str(date_code)[4:6])) for date_code in totals_data[:,0]])
+            date_indices_to_sum = (month_codes == 13)
             for i in range(len(date_indices_to_sum)):
                 if date_indices_to_sum[-i-1] == False:
                     date_indices_to_sum[-i-1] = True
@@ -108,13 +147,13 @@ class EClass:
             totals_data = np.array([np.sum(self.data[date_indices_to_sum],axis=0)])
             totals_data[-1,0] = self.data[-1,0]
         else:
-            raise ValueError('"{}" is not a frequency compatible with this dataset; see documentation for permissible frequencies.'.format(freq))
+            raise ValueError(self.freq_errmsg.format(freq))
         totals_array = totals_data
         return totals_array
         
-    def extrema(self,extremum,freq,start_date,end_date):
+    def extrema(self,extremum,freq,start_date=None,end_date=None):
         """
-        Get the maximum/minimum energy ever consumed over a given interval.
+        Get the maximum/minimum energy consumed over a given period.
         
         Parameters
         ----------
@@ -126,21 +165,46 @@ class EClass:
             The user specified dataset starting and ending dates (both inclusive); 
             acceptable formats are 'YYYYMM', 'YYYY-MM', or 'MM-YYYY'. Dashes ("-") can 
             be substituted for periods ("."), underscores ("_"), or forward slashes ("/").
-        """
-        extremum = extremum.lower()[:3]
-
         
+        Returns
+        -------
+        extremum_array : ndarray
+            A 1x2 array giving the extremum values specified (column 2) and the date of
+            occurrence for that value (column 1).
+        """
+        # Bound data by start and end dates
+        extrema_data = self._daterange(start_date,end_date)
+
+        # Extract data according to frequency
+        if freq == 'monthly':
+            extremum_data = self._monthlydata(extrema_data)
+        elif freq == 'yearly':
+            extremum_data = self._yearlydata(extrema_data)
+        else:
+            raise ValueError(self.freq_errmsg.format(freq))
+        
+        # Select max or min
+        extremum = extremum.lower()[:3]
+        if extremum == 'max':
+            extremum_array = np.array([extremum_data[np.argmax(extremum_data[:,1])]])
+        elif extremum == 'min':
+            extremum_array = np.array([extremum_data[np.argmin(extremum_data[:,1])]])
+        else:
+            raise ValueError(self.extr_errmsg.format(extremum))
+        return extremum_array
+
     #def more_than(self,amount,start_date,end_date,interval):
         """
-        Get data for time interval where more than the given amount of energy was consumed
+        Get data for time interval where more than the given amount of energy was consumed.
         
         Parameters
         ----------
         amount: float
             The lower boundary (exclusive) for which data may be included in the dataset.
         start_date, end_date : str
-            The user specified dataset starting/ending points; 
-            acceptable formats include 'YYYYMMDD', 'MM-DD-YYYY', and 'MM/DD/YYYY'.
+            The user specified dataset starting and ending dates (both inclusive); 
+            acceptable formats are 'YYYYMM', 'YYYY-MM', or 'MM-YYYY'. Dashes ("-") can 
+            be substituted for periods ("."), underscores ("_"), or forward slashes ("/").
         interval : str
             The time intervals considered for extrema comparison ('yearly',or 'monthly').
         """
@@ -151,6 +215,7 @@ class EClass:
     Additonal potential options to add:
         - average yearly energy consumed
         - average seasonal energy consumed
+        - consolidate date range selection and monthly/yearly/cumulative selection into a _formatdata method
     """
  
 
--- tests/test_eclass.py
+++ tests/test_eclass.py
@@ -9,7 +9,7 @@ class TestEClass:
                              [195101,None,5],
                              [195201,1,5],
                              [195201,1,6]],
-                           float)
+                            float)
         # eliminate nan, isolate energy, remove ecode column
         preprocdata = np.array([[195001,1],
                                 [195201,1]],
@@ -20,14 +20,14 @@ class TestEClass:
     def test_FindingInitialDate(self):
         testdata = np.array([[195001,1,5],
                              [195101,1,5]],
-                           float)
+                            float)
         EC = EClass('nuclear',testdata)
         assert EC.idate == 195001
     
     def test_FindingFinalDate(self):
         testdata = np.array([[195001,1,5],
                              [195101,1,5]],
-                           float)
+                            float)
         EC = EClass('nuclear',testdata)
         assert EC.fdate == 195101
      
@@ -45,9 +45,9 @@ class TestEClass:
                                   [195013,7],
                                   [195101,2],
                                   [195102,1]],
-                                float)
+                                 float)
         EC = EClass('nuclear',testdata)
-        assert np.array_equal(EC.daterange(195011,195102),boundedmonths)
+        assert np.array_equal(EC._daterange(195011,195102),boundedmonths)
     
     def test_daterange_BoundingDefaultDates(self):
         testdata = np.array([[195010,1,5],
@@ -59,17 +59,43 @@ class TestEClass:
                              [195103,1,5]],
                             float)
         EC = EClass('nuclear',testdata)
-        assert np.array_equal(EC.daterange(None,None),testdata[:,0:2])
+        assert np.array_equal(EC._daterange(None,None),testdata[:,0:2])
       
+    def test_monthlydata_FindingDataByMonth(self):
+        testdata = np.array([[195013,2,5],
+                             [195101,1,5],
+                             [195102,1,5],
+                             [195113,3,5]],
+                            float)
+        monthlydata = np.array([[195101,1],
+                                  [195102,1]],
+                                 float)
+        EC = EClass('nuclear',testdata)
+        testdata_bounded = EC._daterange(None,None)
+        assert np.array_equal(EC._monthlydata(testdata_bounded),monthlydata)
+        
+    def test_yearlydata_FindingDataByYear(self):
+        testdata = np.array([[195013,2,5],
+                             [195101,1,5],
+                             [195102,1,5],
+                             [195113,3,5]],
+                            float)
+        yearlydata = np.array([[195013,2],
+                                 [195113,3]],
+                                float)
+        EC = EClass('nuclear',testdata)
+        testdata_bounded = EC._daterange(None,None)
+        assert np.array_equal(EC._yearlydata(testdata_bounded),yearlydata)
+        
     def test_totals_FindingMonthlyTotals(self):
         testdata = np.array([[195013,2,5],
                              [195101,1,5],
                              [195102,1,5],
                              [195113,3,5]],
-                           float)
+                            float)
         monthlytotals = np.array([[195101,1],
                                   [195102,1]],
-                                  float)
+                                 float)
         EC = EClass('nuclear',testdata)
         assert np.array_equal(EC.totals(freq='monthly'),monthlytotals)
         
@@ -84,14 +110,64 @@ class TestEClass:
                                 float)
         EC = EClass('nuclear',testdata)
         assert np.array_equal(EC.totals(freq='yearly'),yearlytotals)
-    
+
     def test_totals_FindingCumulativeTotal(self):
         testdata = np.array([[195013,2,5],
                              [195101,1,5],
                              [195113,3,5],
                              [195201,1,5]],
-                           float)
+                            float)
         cumulativetotal = np.array([[195201,6]],
-                                 float)
+                                   float)
         EC = EClass('nuclear',testdata)
         assert np.array_equal(EC.totals(freq='cumulative'),cumulativetotal)
+
+    def test_extrema_FindingMonthlyMaximum(self):
+        testdata = np.array([[195013,6,5],
+                             [195101,1,5],
+                             [195102,4,5],
+                             [195113,5,5],
+                             [195201,1,5]],
+                            float)
+        monthlymax = np.array([[195102,4]],
+                              float)
+        EC = EClass('nuclear',testdata)
+        assert np.array_equal(EC.extrema('max','monthly'),monthlymax)
+
+    def test_extrema_FindingMonthlyMinimum(self):
+        testdata = np.array([[195013,6,5],
+                             [195101,1,5],
+                             [195102,4,5],
+                             [195113,5,5],
+                             [195201,2,5]],
+                            float)
+        monthlymin = np.array([[195101,1]],
+                              float)
+        EC = EClass('nuclear',testdata)
+        assert np.array_equal(EC.extrema('min','monthly'),monthlymin)
+
+    def test_extrema_FindingYearlyMinimum(self):
+        testdata = np.array([[195013,7,5],
+                             [195101,2,5],
+                             [195102,3,5],
+                             [195113,5,5],
+                             [195201,1,5]],
+                            float)
+        yearlymin = np.array([[195113,5]],
+                              float)
+        EC = EClass('nuclear',testdata)
+        assert np.array_equal(EC.extrema('min','yearly'),yearlymin)
+
+    def test_extrema_FindingYearlyMaximum(self):
+        testdata = np.array([[195013,4,5],
+                             [195101,2,5],
+                             [195102,3,5],
+                             [195113,5,5],
+                             [195201,1,5]],
+                            float)
+        yearlymax = np.array([[195113,5]],
+                              float)
+        EC = EClass('nuclear',testdata)
+        assert np.array_equal(EC.extrema('max','yearly'),yearlymax)
+
+
