7b845659c45b3e0b38f04c811dcf1278dda54b0b
mitch <mitchell.negus.57@gmail.com>
2017-09-26 13:06:58 -0700

    added __init__.py with specs on how to find modules

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# Note that tests should be run from test parent directory (package top)
+import os, sys                                                                  
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
