da4b0fb8889ae87a9bf0925e214efc7016b682b1
mitch <mitchell.negus.57@gmail.com>
2017-11-28 09:57:10 -0800

    framework working for dataframes, update tests

--- main/eclass.py
+++ main/eclass.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from utils.load_data import load_dataset
 from utils.eia_codes import name_to_code, date_to_code
@@ -7,7 +6,7 @@ from utils.eia_codes import name_to_code, date_to_code
 class EClass:
     
     """
-    Collect energy consumption data for a user-defined energy source.
+    Collect energy data for a user-defined energy source.
     
     Retrieves data from the specified energy source according to specific 
     attributes, such as energy consumed per decade, per year, or all years in 
@@ -15,7 +14,7 @@ class EClass:
     Use this class to extract and return pure data from the dataset.
     """
     
-    def __init__(self,energy_source,dataset=pd.DataFrame()):
+    def __init__(self,energy_source,stat_type='consumption',data=pd.DataFrame(),data_date='default'):
         """
         Receive energy source and collect corresponding data.
         
@@ -23,110 +22,98 @@ class EClass:
         ----------
         energy_source : str
             The energy source to be pulled from the dataset.
-        dataset : array_like, optional
-            The dataset from which to pull information. Must be three ordered
-            columns–date, energy quantity, and energy code–with no headings.
-            If omitted, use the default dataset.
+        stat_type : str
+            The type of statistic to be collected ('production', 'consumption',
+            'import', or 'export').
+        dataset : DataFrame, optional
+            The dataset from which to pull information. Must be three columns:
+            date, energy quantity, and energy code. If omitted, use the default
+            dataset.
+        dataset_date : str
+            The date identifier of the dataset; 'default' and 'newest' are 
+            current options (the ability to call specific dataset dates to be
+            added).
         """
         # Determine Ecode from energy source name
-        Ecode = name_to_code(energy_source)
+        E_code = name_to_code(energy_source)
 
         # Use default dataset if dataset argument is omitted
-        if dataset.empty:
-            dataset = load_dataset()
+        if data.empty:
+            data = load_dataset(dataset_date=data_date,dataset_type=stat_type)
        
         # Isolate this energy's data from source and remove the Ecode column
-        energy_dataset = dataset[dataset.Ecode == Ecode]
-        self.data = energy_dataset[['Date','Value']]
+        E_data = data[data.E_code == E_code]
+        E_data = E_data[['Date_code','Value']]
 
-        # Eliminate 'nan' data 
-        self.data.dropna(inplace=True)
-        
-        # Separate the data into monthly and yearly sets for this energy
-        self.monthly_data = self.data[self.data.Date.str[-2:]!=13]
-        self.yearly_data = self.data[self.data.Date.str[-2:]==13]
+        # Separate the data into monthly and yearly sets for this energy source
+        self.monthly_data, self.yearly_data = self._sep_month_and_year(E_data)
 
-        # Get the oldest and newest datapoint dates for this energy" 
-        self.idate = self.data.Date.min()
-        self.fdate = self.data.Date.max()
-        
         self.freq_errmsg = 'Frequency "{}" is not compatible with this dataset; see documentation for permissible frequencies.' 
         self.extr_errmsg = 'Input "{}" is not recognized as an extrema; try "max" or "min"' 
 
-    def _daterange(self,start_date,end_date):
+    def _sep_month_and_year(self,data):
+        """
+        Separate the data into monthly and yearly totals
+
+        Returns
+        -------
+        monthly_data : DataFrame
+            A subset of the data with the energy values reported monthly.
+        yearly_data : DataFrame
+            A subset of the data with the energy values reported yearly.
+        """
+        # Monthly totals
+        monthly_data = data[data.Date_code.str[-2:]!='13']
+        monthly_data = monthly_data.assign(Date=monthly_data.Date_code)
+        monthly_data = monthly_data.set_index('Date_code')[['Date','Value']]
+        # Yearly totals
+        yearly_data = data[data.Date_code.str[-2:]=='13']
+        yearly_data = yearly_data.assign(Date=yearly_data.Date_code.str[:-2])
+        yearly_data = yearly_data.set_index('Date_code')[['Date','Value']]
+        return monthly_data,yearly_data
+
+    def _daterange(self,data,start_date,end_date):
         """
         Resize the dataset to cover only the date range specified.
         
         Parameters
         ----------
+        freq : str
+            The frequency for gathering totals ('monthly','yearly',or
+            'cumulative').
         start_date, end_date : int
             The dataset start/end dates (both inclusive) as integers (YYYYMM).
             
         Returns
         -------
-        bound_data : ndarray
-            A 2 column array corresponding to the specified date range.
+        bound_data : DataFrame
+            A dataframe corresponding to the specified date range.
         """
         # Use dataset default dates unless otherwise specified by the user
-        if start_date == None: start_date = self.idate
-        else: date_to_code(str(start_date))
-        if end_date == None: end_date = self.fdate
-        else: date_to_code(str(end_date))
-       
+        if start_date == None: start_date = data.index.min()
+        else: start_date = date_to_code(start_date) 
+        if end_date == None: end_date = data.index.max()
+        else: end_date = date_to_code(end_date)
+
         # Adjust dataset boundaries 
-        half_bounded_data = self.data[self.data[:,0] >= start_date]
-        bounded_data = half_bounded_data[half_bounded_data[:,0] <= end_date]
+        half_bounded_data = data[data.index >= start_date]
+        bounded_data = half_bounded_data[half_bounded_data.index <= end_date]
         return bounded_data
-     
-    def _monthlydata(self,data):
-        """
-        Limit the data to consider only monthly totals.
 
-        Parameters
-        ----------
-        data : ndarray
-            A 2 column array with data to be filtered by month.
-
-        Returns
-        -------
-        monthly_data : ndarray
-            A 2 column array with data filtered into monthly intervals.
-        """
-        # Remove dates where "date code" digits 5 and 6 are not 01-12
-        month_codes = [int(float(str(code)[4:6])) for code in data[:,0]] 
-        month_codes = np.array(month_codes)
-        monthly_data = data[month_codes!=13]
-        return monthly_data
-     
-    def _yearlydata(self,data):
-        """
-        Limit the data to consider only yearly totals.
-
-        Parameters
-        ----------
-        data : ndarray
-            A 2 column array with data to be filtered by year.
-
-        Returns
-        -------
-        yearly_data : ndarray
-            A 2 column array corresponding to only the yearly totals.
-        """
-        # Remove dates where "date code" digits 5 and 6 are not 13
-        month_codes = [int(float(str(code)[4:6])) for code in data[:,0]] 
-        month_codes = np.array(month_codes)
-        yearly_data = data[month_codes==13]
-        return yearly_data
-       
     def totals(self,freq='yearly',start_date=None,end_date=None,):
         """
-        Get the energy consumption totals over a given period.
+        Get the energy statistic totals over a given period.
         
-        * Note: the 'cumulative' option includes the entire year of the
-                `start_date` specified (i.e. if `start_date` = '201508;, the
-                cumulative sum will account for the entirety of 2015,
-                including all months before August.
-
+        This method aggregates energy statistic totals according to a user 
+        defined frequency--either monthly, yearly, or cumulatively. Data is
+        collected for the entire dataset unless specific dates are given.
+        When dates are provided, the totals are only returned on that time 
+        interval, with inclusive starting and ending dates. If data at the
+        specified frequency does not exist for the entire interval, the interval
+        will be automatically adjusted to fit the available data in the 
+        interval. Cumulative totals use yearly data, and so only include data up
+        until the last complete year.
+        
         Parameters
         ----------
         freq : str
@@ -134,88 +121,73 @@ class EClass:
             'cumulative').
         start_date, end_date : str
             The user specified starting and ending dates for the dataset 
-            (both inclusive); acceptable formats are 'YYYYMM', 'YYYY-MM',
-            or 'MM-YYYY'. Dashes can be substituted for periods, underscores,
-            or forward slashes.
+            (both inclusive); for 'monthly', acceptable formats are 'YYYYMM',
+            'YYYY-MM', or 'MM-YYYY' (dashes can be substituted for periods,
+            underscores, or forward slashes); for 'yearly' or 'cumulative',
+            give only the full year, 'YYYY'.
             
         Returns
         -------
-        totals_array : ndarray
-            A 2 column array giving energy totals for a series of dates in
-            the given interval and at the given frequency.
+        totals_data : DataFrame, float
+            A dataframe containing totals in the specified interval at the 
+            given frequency, a floating point number if a cumulative sum.
         """
-        freq = freq.lower()
-
-        # Bound data by start and end dates
-        totals_data = self._daterange(start_date,end_date)
-        
-        # Extract data according to frequency
+        # Bound data at requested frequency by start and end dates
         if freq == 'monthly':
-            totals_data = self._monthlydata(totals_data)
-        elif freq == 'yearly':
-            totals_data = self._yearlydata(totals_data)
-        elif freq == 'cumulative':
-            # Create a numpy array (1x2) of the energy source's cumulative total
-            month_codes = [int(float(str(code)[4:6])) for code in totals_data[:,0]] 
-            month_codes = np.array(month_codes)
-            date_indices_to_sum = (month_codes == 13)
-            # Add dates from last reported year through given end date
-            for i in range(len(date_indices_to_sum)):
-                if date_indices_to_sum[-i-1] == False:
-                    date_indices_to_sum[-i-1] = True
-                else: break
-            cum_sum = np.sum(totals_data[date_indices_to_sum,1],axis=0)
-            # Use the last date considered for the date
-            totals_data = np.array([[totals_data[-1,0],cum_sum]])
-            print(totals_data)
+            full_data = self.monthly_data
+        elif freq == 'yearly' or freq == 'cumulative':
+            full_data = self.yearly_data
         else:
             raise ValueError(self.freq_errmsg.format(freq))
-        totals_array = totals_data
-        return totals_array
+        totals_data = self._daterange(full_data,start_date,end_date)
+
+        # For cumulative totals, take the sum
+        if freq == 'cumulative':
+            totals_data = totals_data.Value.sum()
+        return totals_data
         
-    def extrema(self,extremum,freq,start_date=None,end_date=None):
+    def extrema(self,extremum,freq='monthly',start_date=None,end_date=None):
         """
         Get the maximum/minimum energy consumed over a given period.
         
         Parameters
         ----------
         extremum : str
-            The exteme value to be found ('max' or 'min).
+            The exteme value to be found ('max' or 'min').
         freq : str
             The frequency for checking extrema ('monthly' or 'yearly').
         start_date, end_date : str
             The user specified starting and ending dates for the dataset 
-            (both inclusive); acceptable formats are 'YYYYMM', 'YYYY-MM',
-             or 'MM-YYYY'. Dashes can be substituted for periods, underscores,
-             or forward slashes.
+            (both inclusive); for 'monthly', acceptable formats are 'YYYYMM',
+            'YYYY-MM', or 'MM-YYYY' (dashes can be substituted for periods,
+            underscores, or forward slashes); for 'yearly' or 'cumulative',
+            give only the full year, 'YYYY'.
         
         Returns
         -------
-        extremum_array : ndarray
-            A 1x2 array giving the extremum values specified (column 2) and the
-            date of occurrence for that value (column 1).
+        extremum_data : DataFrame
+            A dataframe giving the specified extreme value and the date of
+            occurrence for that value.
         """
         # Bound data by start and end dates
-        extrema_data = self._daterange(start_date,end_date)
-
-        # Extract data according to frequency
         if freq == 'monthly':
-            extremum_data = self._monthlydata(extrema_data)
+            full_data = self.monthly_data
         elif freq == 'yearly':
-            extremum_data = self._yearlydata(extrema_data)
+            full_data = self.yearly_data
         else:
             raise ValueError(self.freq_errmsg.format(freq))
-        
+        extremum_data = self._daterange(full_data,start_date,end_date)
+
         # Select max or min
         extremum = extremum.lower()[:3]
         if extremum == 'max':
-            extremum_array = [extremum_data[np.argmax(extremum_data[:,1])]]
+            extremum_val = extremum_data.Value.max()
         elif extremum == 'min':
-            extremum_array = [extremum_data[np.argmin(extremum_data[:,1])]]
+            extremum_val = extremum_data.Value.min()
         else:
             raise ValueError(self.extr_errmsg.format(extremum))
-        extremum_array = np.array(extremum_array)
-        return extremum_array
+        extremum_data = extremum_data[extremum_data.Value == extremum_val]
+        return extremum_data
 
     #def more_than(self,amount,start_date,end_date,interval):
         """
