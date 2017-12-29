d5cef39f9c5943ec49fe3bcb6ade7b90882af55e
mitch <mitchell.negus.57@gmail.com>
2017-12-28 20:56:17 -0500

    an example of the visualization tool

---
+++ example.py
@@ -0,0 +1,8 @@
+from main.visuals import Visual
+from matplotlib import pyplot as plt
+
+visual = Visual()
+visual.include_energy('nuclear','coal')
+#visual.linegraph('totals')
+visual.linegraph('totals',freq='monthly')
+plt.show()
