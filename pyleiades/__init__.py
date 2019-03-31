b95e53c768cf44cf0f557d5216d8fd600062ef7f
mitch <mitchell.negus.57@gmail.com>
2019-03-30 22:14:29 -0700

    moved package level constants into __init__

--- pyleiades/__init__.py
+++ pyleiades/__init__.py
@@ -1,1 +1,10 @@
+# Licensed under the FreeBSD license
+
+"""PyLEIADEs: tools to use with the EIA Monthly Energy Review datasets."""
+
+__version__ = '0.1.dev'
+
+DATA_DIR = 'data'
+ARCHIVE_DIR = f'{DATA_DIR}/archive'
+
 
