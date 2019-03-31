cf7d3c0930b46b195579824e4b9f72bffc885328
mitch <mitchell.negus.57@gmail.com>
2019-03-31 14:56:06 -0700

    single source version location

--- pyleiades/__init__.py
+++ pyleiades/__init__.py
@@ -11,13 +11,18 @@ Data can be accessed directly at the EIA website:
     https://www.eia.gov/totalenergy/data/browser/
 """
 
-__version__ = '0.1.dev'
-
 import os
 from pyleiades.energies import Energy
 from pyleiades.visuals import Visual
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+package_dir = os.path.dirname(__file__)
+project_dir = os.path.join(package_dir, '..')
+
+# Read the contents of the VERSION file
+with open(os.path.join(project_dir, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+DATA_DIR = os.path.join(package_dir, 'data')
 ARCHIVE_DIR = os.path.join(DATA_DIR, 'archive')
 
 
--- setup.py
+++ setup.py
@@ -10,22 +10,30 @@ Energy Information Administration (EIA) Monthly Energy Review (MER) datasets.
 Data can be accessed directly at the EIA website:
     https://www.eia.gov/totalenergy/data/browser/
 """
-
-DOCLINES = (__doc__ or '').split("\n")
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+from os import path
+
+project_dir = path.abspath(path.dirname(__file__))
+# Read the contents of the VERSION file
+with open(os.path.join(project_dir, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+# Read the contents of the README file
+with open(path.join(project_dir, 'README.md'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name='pyleiades',
-    version='0.0.dev',
+    version=version,
     description='An API for examing the EIA Monthly Energy Review datasets.',
     author='Mitch Negus',
     author_email='mitchell.negus.17@gmail.com',
     license='FreeBSD',
-    long_description='\n'.join(DOCLINES),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     url='https://github.com/mitchnegus/pyleiades',
     packages=['pyleiades', 'pyleiades.utils'],
     scripts=['scripts/update_eia_data.py'],
