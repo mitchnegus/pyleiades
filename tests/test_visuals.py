f0cbf4e7e54b77f2b345ea9d3c89530ba4f2fe12
mitch <mitchell.negus.57@gmail.com>
2019-03-31 21:22:59 -0700

    added tests for visualization type initializations

--- tests/test_visuals.py
+++ tests/test_visuals.py
@@ -5,6 +5,25 @@ from pyleiades.visuals import Visual
 
 class TestVisual:
 
+    def test_initialze_with_string(self, testdata):
+        nuc_test_data = testdata.iloc[7:].value
+        visual = Visual('nuclear', data=testdata)
+        nuc_energy_data = visual.energies[0].energy_data.value
+        assert 'nuclear' == visual.energies[0].energy_type
+        assert nuc_energy_data.equals(nuc_test_data)
+
+    def test_initialze_with_list(self, testdata):
+        nuc_test_data = testdata.iloc[7:].value
+        visual = Visual(['nuclear', 'coal'], data=testdata)
+        nuc_energy_data = visual.energies[0].energy_data.value
+        assert 'nuclear' == visual.energies[0].energy_type
+        assert 'coal' == visual.energies[1].energy_type
+        assert nuc_energy_data.equals(nuc_test_data)
+
+    def test_initialze_invalid(self, testdata):
+        with pytest.raises(ValueError):
+            visual = Visual(123, data=testdata)
+
     def test_include_energy(self, testdata):
         nuc_test_data = testdata.iloc[7:].value
         visual = Visual(data=testdata)
