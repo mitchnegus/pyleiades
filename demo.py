ebad6463976349241b6d94a26f28042ee32c0093
mitch <mitchell.negus.57@gmail.com>
2019-03-31 17:17:04 -0700

    added legends to plots

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@ from matplotlib import pyplot as plt
 
 visual = Visual()
 visual.include_energy('nuclear','coal')
-#visual.linegraph('totals')
-visual.linegraph('totals', freq='monthly')
+visual.linegraph('totals')
+#visual.linegraph('totals', freq='monthly')
 plt.show()
